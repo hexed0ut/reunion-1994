cca78e958f391b1910b38b5b45bbfabd691611c9
Abesh Roy <102050935+hexed0ut@users.noreply.github.com>
2025-11-25 01:27:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ form.addEventListener("submit", async (e) => {
   loading.classList.add("hidden");
   successMsg.classList.remove("hidden");
   addAnotherBtn.classList.remove("hidden");
+  submitBtn.style.display = 'none';
 
   form.reset();
   nonvegOptions.classList.add("hidden");
@@ -59,6 +60,7 @@ addAnotherBtn.addEventListener("click", () => {
   successMsg.classList.add("hidden");
   addAnotherBtn.classList.add("hidden");
 });
+
 
 
 
